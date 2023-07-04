60acef0678199f2a440b7928bda59a1e0145b605
Ivan Marinski <noobdrew23@gmail.com>
2023-07-04 20:18:05 +0300

    have to create answer components

--- src/QuizBody.jsx
+++ src/QuizBody.jsx
@@ -4,23 +4,33 @@ import { decode } from "html-entities";
 import { nanoid } from "nanoid";
 
 export default function QuizBody(props) {
-  console.log(props.apiData);
-  const quizElements = props.apiData.map((question) => {
+  const [selectedAnswers, setSelectedAnswers] = useState([1, 2, 3, 4, 5]);
+  const [allQuestions, setAllQuestions] = useState(props.apiData);
+
+  console.log(allQuestions);
+  const quizElements = allQuestions.map((question) => {
     return (
       <QuizElement
+        correct={question.correct_answer}
         key={question.id}
+        id={question.id}
         questionText={question.question}
         question={question}
         answer={question.allAnswers}
+        setAnswer={setAnswer}
       />
     );
   });
+
+  function setAnswer(selected, id) {}
 
   function checkAnswers() {}
   return (
     <div className="quiz-body">
       <div className="quiz-container">{quizElements}</div>
-      <button className="check-answers">Check answers</button>
+      <button className="check-answers" onClick={checkAnswers}>
+        Check answers
+      </button>
     </div>
   );
 }
--- src/QuizElement.jsx
+++ src/QuizElement.jsx
@@ -1,50 +1,19 @@
 import { decode } from "html-entities";
+import { useState } from "react";
 
 export default function QuizElement(props) {
+  const [value, setValue] = useState("");
+
+  function setCurrentValue(input) {
+    setValue(input.target._wrapperState.initialValue);
+  }
+  console.log(value);
   return (
     <>
       <div className="quiz-element">
         <h3 className="question">{decode(props.questionText)}</h3>
-        <ul className="answers">
-          <li>
-            <input
-              type="radio"
-              name={props.question.question}
-              id={props.answer[0]}
-              value={props.answer[0]}
-              className="answer-button "
-            />
-            <label htmlFor={props.answer[0]}> {decode(props.answer[0])}</label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              name={props.question.question}
-              value={props.answer[1]}
-              id={props.answer[1]}
-              className="answer-button "
-            />
-            <label htmlFor={props.answer[1]}> {decode(props.answer[1])}</label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              name={props.question.question}
-              id={props.answer[2]}
-              className="answer-button "
-            />
-            <label htmlFor={props.answer[2]}> {decode(props.answer[2])}</label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              name={props.question.question}
-              id={props.answer[3]}
-              className="answer-button active"
-            />
-            <label htmlFor={props.answer[3]}> {decode(props.answer[3])}</label>
-          </li>
-        </ul>
+
+        <ul className="answers"></ul>
       </div>
       <hr />
     </>
