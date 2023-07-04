f79cc8f1d1f7c2b51e01c79e154183d4960ae89d
Ivan Marinski <noobdrew23@gmail.com>
2023-07-04 13:33:47 +0300

    quiz start component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import Quiz from "./Quiz";
+import "./App.css";
+import { decode } from "html-entities";
+// fetch("https://opentdb.com/api.php?amount=5")
+//   .then((resp) => resp.json())
+//   .then((data) => console.log(data));
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [quiz, setQuiz] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Quiz />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Quiz.jsx
@@ -0,0 +1,9 @@
+export default function Quiz() {
+  return (
+    <div className="start-screen">
+      <h1 className="title">Quizzical</h1>
+      <p className="sub-title">Test your knowledge!</p>
+      <button className="start-quiz">Start quiz</button>
+    </div>
+  );
+}
