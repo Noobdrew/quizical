d65521bf3aea021faf2b71ab0e2ab94f78563b4e
Ivan Marinski <noobdrew23@gmail.com>
2023-07-05 14:00:01 +0300

    Revert "have to create answer components"
    This reverts commit 60acef0678199f2a440b7928bda59a1e0145b605.

--- src/QuizBody.jsx
+++ src/QuizBody.jsx
@@ -4,28 +4,28 @@ import { decode } from "html-entities";
 import { nanoid } from "nanoid";
 
 export default function QuizBody(props) {
-  const [allQuestions, SetAllQuestions] = useState(props.apiData);
+  const [selectedAnswers, setSelectedAnswers] = useState([1, 2, 3, 4, 5]);
+  const [allQuestions, setAllQuestions] = useState(props.apiData);
 
+  console.log(allQuestions);
   const quizElements = allQuestions.map((question) => {
     return (
       <QuizElement
-        correct={question.correct_answer}
         key={question.id}
-        id={question.id}
         questionText={question.question}
         question={question}
         answer={question.allAnswers}
       />
     );
   });
-  console.log(allQuestions);
+
+  function setAnswer(selected, id) {}
+
   function checkAnswers() {}
   return (
     <div className="quiz-body">
       <div className="quiz-container">{quizElements}</div>
-      <button className="check-answers" onClick={checkAnswers}>
-        Check answers
-      </button>
+      <button className="check-answers">Check answers</button>
     </div>
   );
 }
